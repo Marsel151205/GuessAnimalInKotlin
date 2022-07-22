d0fcf18280011a063bd59ac0531f8d51ca4aba7b
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-07-22 12:31:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson5/ResultActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lesson5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lesson5.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val result = intent.getStringExtra("key")
+        binding.tvResult.text = result
+    }
+}
